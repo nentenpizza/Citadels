1810a109d3af9a1ee8779eb4cb1b8e686aa51d0e
nentenpizza <keked@ukr.net>
2021-11-05 17:25:21 +0200

    repository/postgres: implement UsersRepository

--- internal/repository/postgres/users.go
+++ internal/repository/postgres/users.go
@@ -11,17 +11,31 @@ func NewUsersRepository(db *DB) *UsersRepository {
 }
 
 func (u UsersRepository) Create(s string) (int64, error) {
-	panic("implement me")
+	const q = `insert into users (name) values ($1) returning id`
+	rows, err := u.db.Query(q)
+	if err != nil {
+		return 0, err
+	}
+	var id int64
+	if rows.Next() {
+		if err := rows.Scan(&id); err != nil {
+			return id, err
+		}
+	}
+	return id, nil
 }
 
-func (u UsersRepository) ByID(i int64) (domain.User, error) {
-	panic("implement me")
+func (u UsersRepository) ByID(id int64) (user domain.User, _ error) {
+	const q = `select * from users where id = $1`
+	return user, u.db.Get(&user, q, id)
 }
 
-func (u UsersRepository) ByName(s string) (domain.User, error) {
-	panic("implement me")
+func (u UsersRepository) ByName(name string) (user domain.User, _ error) {
+	const q = `select * from users where name = $1`
+	return user, u.db.Get(&user, q, name)
 }
 
-func (u UsersRepository) ExistsByName(s string) (bool, error) {
-	panic("implement me")
+func (u UsersRepository) ExistsByName(name string) (has bool, _ error) {
+	const q = `select exists(*) from users where name = $1`
+	return has, u.db.Get(&has, q, name)
 }
